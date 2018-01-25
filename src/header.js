56f3fab4cacf7d349ebcf1efd7145b339a057919
T-Dex <dex.m.mills@gmail.com>
2018-01-25 16:48:04 -0700

    Log In UI

--- src/header.js
+++ src/header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import mainLogo from './styles/logos/mainlogo2.png';
+import './styles/app.css';
 
 
 class Header extends Component{
@@ -8,15 +9,13 @@ class Header extends Component{
   }
   render(){
     return(
-      <div>
-      <header className="header">
-      <div className="header_title"/>
-      <h1 className="title">Cloudveil Heli</h1>
-      <img className="mainLogo" src={mainLogo}/>
-      <div className="userButton">
-        <button className="userLogIn">Log Out</button>
-      </div>
-      </header>
+      <div className="header">
+          <div className="header_title"/>
+          <h1 className="title">Cloudveil Heli</h1>
+          <img className="mainLogo" src={mainLogo}/>
+          <div className="userButton">
+            <button className="userLogIn">Log Out</button>
+          </div>
       </div>
     )
   }
--- src/routes/logIn.js
+++ src/routes/logIn.js
@@ -8,9 +8,13 @@ class logIn extends Component{
   }
   render(){
     return(
-      <div>
-        <h1>Log In</h1>
-      </div>
+      <div className="loginPage">
+        <h1>BlueBird Heli</h1>
+        <h2>Log In</h2>
+        <div className="loginFields"><input className="Email" type="text"/>Email
+        <input className="password" type="password"/>Password</div>
+        <div><button>Enter</button></div>
+        </div>
     )
   }
 }
