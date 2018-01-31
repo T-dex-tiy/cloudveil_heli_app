c19180da2040f704b31904e1f17e287ebf4ad639
T-Dex <dex.m.mills@gmail.com>
2018-01-30 17:28:35 -0700

    Routing...I think

--- src/routes/logIn.js
+++ src/routes/logIn.js
@@ -10,26 +10,39 @@ class logIn extends Component{
     super(props)
     this.state={
       uid:null,
-      onwer:null
+      owner:null
     }
   }
+
+componentWillMount(){
+
+}
 
 renderLogin(){
   const pass= this.refs.pass.value;
   const email= this.refs.email.value;
-  auth.signInWithEmailAndPassword(email, pass)
+  const promise=auth.signInWithEmailAndPassword(email, pass);
+  promise.catch(console.log(auth.message))
+  // console.log("promise",promise)
+  console.log("auth displayName",promise.currentUser);
+  // auth().onAuthStateChanged(firebaseUser=>{
+  //   if(firbaseUser){
+  //     console.log(firebaseUser)
+  //   }
+  //   else{
+  //     console.log("not logged in");
+  //   }
+  // })
 }
 
   render(){
-    var email=this.refs.email;
-    var pass=this.refs.pass;
     return(
       <div className="loginPage">
         <h1>BlueBird Heli</h1>
         <h2>Log In</h2>
         <div className="loginFields"><input className="Email" type="text" ref="email"/>Email
         <input className="password" type="password" ref="pass"/>Password</div>
-        <div><button onClick={()=>this.renderLogin('email', 'pass')}>Enter</button></div>
+        <div><button onClick={()=>this.renderLogin('email', 'pass')}>Click to Enter</button></div>
         </div>
     )
   }
