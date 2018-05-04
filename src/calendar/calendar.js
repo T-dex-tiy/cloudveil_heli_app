016665b6c130bcc2d9def163130d96f7a8b37b01
T-Dex <dex.m.mills@gmail.com>
2018-05-04 07:53:27 -0600

    Added calenedar piping

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,144 +1,251 @@
-import React, { Component } from 'react';
-import moment from 'moment';
+import React, { Component } from "react";
+import moment from "moment";
 
-
-class Calendar extends Component{
-  state={
-    dateContext:moment(),
-    today:moment(),
+class Calendar extends Component {
+  state = {
+    dateContext: moment(),
+    today: moment(),
     showMonthPopUp: false,
     showYearPopup: false,
-
-  }
-  constructor(props){
+    showYearEdit: false
+  };
+  constructor(props) {
     super(props);
-    this.width= props.width || '350px';
-    this.style=props.style || {};
-    this.style.width=this.width;
+    this.width = props.width || "350px";
+    this.style = props.style || {};
+    this.style.width = this.width;
   }
 
-  weekdays= moment.weekdays();
-  weekdaysShort= moment.weekdaysShort();
-  months= moment.months();
+  weekdays = moment.weekdays();
+  weekdaysShort = moment.weekdaysShort();
+  months = moment.months();
 
-  year=()=>{
-    return this.state.dateContext.format("Y")
-  }
-  month=()=>{
-    return this.state.dateContext.format("MMMM")
-  }
+  year = () => {
+    return this.state.dateContext.format("Y");
+  };
+  month = () => {
+    return this.state.dateContext.format("MMMM");
+  };
   daysInMonth = () => {
-       return this.state.dateContext.daysInMonth();
-   }
-  currentDate=()=>{
-    return this.state.dateContext.get('date')
-  }
-  currentDay=()=>{
-    return this.state.dateContext.format("D")
-  }
-  firstDayOfMonth=()=>{
-    let dateContext=this.state.dateContext;
-    let firstDay= moment(dateContext).startOf('month').format('d')
-    return firstDay
-  }
-  SelectList=(props)=>{
-    let popup= props.data.map((data)=>{
-      return(
+    return this.state.dateContext.daysInMonth();
+  };
+  currentDate = () => {
+    return this.state.dateContext.get("date");
+  };
+  currentDay = () => {
+    return this.state.dateContext.format("D");
+  };
+  firstDayOfMonth = () => {
+    let dateContext = this.state.dateContext;
+    let firstDay = moment(dateContext)
+      .startOf("month")
+      .format("d");
+    return firstDay;
+  };
+  setMonth = month => {
+    let headMonth = this.months.indexOf(month);
+    let dateContext = Object.assign({}, this.state.dateContext);
+    dateContext = moment(dateContext).set("month", headMonth);
+    this.setState({
+      dateContext: dateContext
+    });
+  };
+
+  nextMonth = () => {
+    let dateContext = Object.assign({}, this.state.dateContext);
+    dateContext = moment(dateContext).add(1, "month");
+    this.setState({
+      dateContext: dateContext
+    });
+    this.props.onNextMonth && this.props.onNextMonth();
+  };
+  prevMonth = () => {
+    let dateContext = Object.assign({}, this.state.dateContext);
+    dateContext = moment(dateContext).subtract(1, "month");
+    this.setState({
+      dateContext: dateContext
+    });
+    this.props.onPreviousMonth && this.props.onPreviousMonth();
+  };
+  onSelectChange = (e, data) => {
+    this.setMonth(data);
+    this.props.onMonthChange && this.props.onChangeMonth();
+  };
+  SelectList = props => {
+    let popup = props.data.map(data => {
+      return (
         <div key={data}>
-          <a href="#">
-          {data}
+          <a
+            href="#"
+            onClick={e => {
+              this.onSelectChange(e, data);
+            }}
+          >
+            {data}
           </a>
         </div>
-      )
-    })
-    return(
-      <div className="month-popup">
-        {popup}
-      </div>
-    )
-  }
-  onChangeMonth=(e, month)=>{
+      );
+    });
+    return <div className="month-popup">{popup}</div>;
+  };
+  onChangeMonth = (e, month) => {
     this.setState({
-      showMonthPopUp:!this.state.showMonthPopUp
-    })
-  }
-  MonthNav=()=>{
-    return(
-      <span className="label-month" onClick ={(e)=>{this.onChangeMonth(e,this.month())}}>
+      showMonthPopUp: !this.state.showMonthPopUp
+    });
+  };
+  MonthNav = () => {
+    return (
+      <span
+        className="label-month"
+        onClick={e => {
+          this.onChangeMonth(e, this.month());
+        }}
+      >
         {this.month()}
-        {this.state.showMonthPopUp && <this.SelectList data={this.months}/>}
-
+        {this.state.showMonthPopUp && <this.SelectList data={this.months} />}
       </span>
-    )
-  }
-
-  render(){
-    const weekdays=this.weekdaysShort.map((day)=>{
-      return(
-        <td key={day} className='week-day'>{day}</td>
-      )
-    })
-    const blanks=[];
-    for(let i=0;i<this.firstDayOfMonth();i++){
-      blanks.push(<td key={i*666} className="emptySlot">{""}</td>)
+    );
+  };
+  setYear = year => {
+    let dateContext = Object.assign({}, this.state.dateContext);
+    dateContext = moment(dateContext).set("year", year);
+    this.setState({
+      dateContext: dateContext
+    });
+  };
+  onYearChange = e => {
+    this.setYear(e.target.value);
+    this.props.onYearChange && this.props.onYearChange(e, e.target.value);
+  };
+  showYearEdit = () => {
+    this.setState({
+      showYearEdit: true
+    });
+  };
+  onKeyUpYear = e => {
+    if (e.which === 13 || e.which === 27) {
+      this.setYear(e.target.value);
+      this.setState({
+        showYearEdit: false
+      });
+    }
+  };
+  YearNav = () => {
+    return this.state.showYearEdit ? (
+      <input
+        defaultValue={this.year()}
+        className="editor-year"
+        ref={yearInput => {
+          this.yearInput = yearInput;
+        }}
+        onKeyUp={e => this.onKeyUpYear(e)}
+        onChange={e => this.onYearChange(e)}
+        type="number"
+        placeholder="year"
+      />
+    ) : (
+      <span
+        className="label-year"
+        onDoubleClick={e => {
+          this.showYearEdit();
+        }}
+      >
+        {this.year()}
+      </span>
+    );
+  };
+  onDayClick = (e, day) => {
+    this.props.onDayClick && this.props.onDayClick(e, day);
+  };
+  render() {
+    const weekdays = this.weekdaysShort.map(day => {
+      return (
+        <td key={day} className="week-day">
+          {day}
+        </td>
+      );
+    });
+    const blanks = [];
+    for (let i = 0; i < this.firstDayOfMonth(); i++) {
+      blanks.push(
+        <td key={i * 666} className="emptySlot">
+          {""}
+        </td>
+      );
     }
 
     let daysInMonth = [];
-           for (let day = 1; day <= this.daysInMonth(); day++) {
-               let className = (day == this.currentDay() ? "day current-day": "day");
-               let selectedClass = (day == this.state.selectedDay ? " selected-day " : "")
-               daysInMonth.push(
-                   <td key={day} className={className + selectedClass} >
-                       <span onClick={(e)=>{this.onDayClick(e, day)}}>{day}</span>
-                   </td>
-               );
-           }
-
-    var totalDays=[...blanks, ...daysInMonth];
-    let rows =[];
-    let cells=[];
-
-    totalDays.forEach((row, i)=>{
-      if((i%7)!==0){
-        cells.push(row);
-      }else{
-      let newRow =cells.slice();
-      rows.push(newRow);
-      cells=[];
-      cells.push(row);
+    for (let day = 1; day <= this.daysInMonth(); day++) {
+      let className = day == this.currentDay() ? "day current-day" : "day";
+      let selectedClass = day == this.state.selectedDay ? " selected-day " : "";
+      daysInMonth.push(
+        <td key={day} className={className + selectedClass}>
+          <span
+            onClick={e => {
+              this.onDayClick(e, day);
+            }}
+          >
+            {day}
+          </span>
+        </td>
+      );
     }
-    if(i===totalDays.length-1){
-      let newRow=cells.slice();
-      rows.push(newRow);
-    }});
 
+    var totalDays = [...blanks, ...daysInMonth];
+    let rows = [];
+    let cells = [];
 
-    const calElements=rows.map((day,index)=>{
-      return (
-        <tr key={index*100}>{day}</tr>
-      )
+    totalDays.forEach((row, i) => {
+      if (i % 7 !== 0) {
+        cells.push(row);
+      } else {
+        let newRow = cells.slice();
+        rows.push(newRow);
+        cells = [];
+        cells.push(row);
+      }
+      if (i === totalDays.length - 1) {
+        let newRow = cells.slice();
+        rows.push(newRow);
+      }
+    });
+
+    const calElements = rows.map((day, index) => {
+      return <tr key={index * 100}>{day}</tr>;
     });
     console.log(blanks);
     console.log(daysInMonth);
-    return(
+    return (
       <div className="calendar-container" style={this.style}>
         <table className="calendar">
           <thead>
             <tr className="calendar-header">
               <td colSpan="5">
-                <this.MonthNav/>
+                <this.MonthNav /> <this.YearNav />
+              </td>
+              <td colSpan="2" className="nav-month">
+                <i
+                  className="prev fa fa-fw fa-chevron-left"
+                  onClick={e => {
+                    this.prevMonth();
+                  }}
+                />
+                <i
+                  className="prev fa fa-fw fa-chevron-right"
+                  onClick={e => {
+                    this.nextMonth();
+                  }}
+                />
               </td>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              {weekdays}
-            </tr>
+            <tr>{weekdays}</tr>
             {calElements}
           </tbody>
         </table>
       </div>
-    )
+    );
   }
 }
 
--- src/calendar/datePicker.js
+++ src/calendar/datePicker.js
@@ -1,122 +1,179 @@
-import React, { Component } from 'react';
-import Reservation from './reservations';
-import Calendar from './calendar'
+import React, { Component } from "react";
+import Reservation from "./reservations";
+import Calendar from "./calendar";
 
-const style={
-  position:'relative',
-  margin:'50px auto'
-}
+const style = {
+  position: "relative",
+  margin: "50px auto"
+};
 
-class ResPage extends Component{
-  constructor(props){
+class ResPage extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      date:"",
+      date: ""
     };
-    this.newRes=this.newRes.bind(this)
-    this.onChange=this.onChange.bind(this)
+    this.newRes = this.newRes.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
-onChange(event) {
-  console.log(this, "On Change");
-  console.log(this.getDate);;
-  const val = event;
-  let newDate = val;
-  this.setState({date:newDate})
-}
+  onChange(event) {
+    console.log(this, "On Change");
+    console.log(this.getDate);
+    const val = event;
+    let newDate = val;
+    this.setState({ date: newDate });
+  }
 
-getDate(event){
-  console.log(this.new);
-}
+  getDate(event) {
+    console.log(this.new);
+  }
 
-newRes(event){
-  var pickUpLocation=this.refs.hangarPickup.value;
-  if(pickUpLocation=="Other"){
-    let pickUpZone=prompt("Enter Location for Pick up");
-    pickUpLocation=pickUpZone;
-    const Res={
-      opsArea:this.refs.opsArea.value,
-      hangarPickup:pickUpLocation,
-      guest:this.refs.guest.value,
-      day:this.state.date,
-      time:this.refs.pickupTime.value
+  newRes(event) {
+    var pickUpLocation = this.refs.hangarPickup.value;
+    if (pickUpLocation == "Other") {
+      let pickUpZone = prompt("Enter Location for Pick up");
+      pickUpLocation = pickUpZone;
+      const Res = {
+        opsArea: this.refs.opsArea.value,
+        hangarPickup: pickUpLocation,
+        guest: this.refs.guest.value,
+        day: this.state.date,
+        time: this.refs.pickupTime.value
+      };
+      this.props.newReservation(Res);
+      console.log(pickUpZone, pickUpLocation);
+      return;
+    } else {
+      const Res = {
+        opsArea: this.refs.opsArea.value,
+        hangarPickup: pickUpLocation,
+        guest: this.refs.guest.value,
+        day: this.state.date,
+        time: this.refs.pickupTime.value
+      };
+      console.log(Res.hangarPickup);
+      this.props.newReservation(Res);
+      alert("Get ready to Fly ");
     }
-    this.props.newReservation(Res)
-    console.log(pickUpZone, pickUpLocation);
-    return
-  }else{
-
-  const Res={
-    opsArea:this.refs.opsArea.value,
-    hangarPickup:pickUpLocation,
-    guest:this.refs.guest.value,
-    day:this.state.date,
-    time:this.refs.pickupTime.value
   }
-  console.log(Res.hangarPickup);
-  this.props.newReservation(Res);
-  alert("Get ready to Fly ")
-}}
 
-onClickYear(event){
-  const val= event;
-  console.log("Clicked Year: ", val)
-}
-
-  render(){
+  onClickYear(event) {
+    const val = event;
+    console.log("Clicked Year: ", val);
+  }
+  onDayClick = (e, day) => {
+    console.log(day);
+  };
+  render() {
     console.log(this.props.users);
     console.log(this.props.reservations);
-    let day="";
+    let day = "";
     console.log(day);
-    return(
+    return (
       <div className="selectionpage">
-      <div className="selection">
-        <select className="dropdownStyle"ref="opsArea">
-          <option className="dropdownStyle" value=""disable="true" selected hidden>Choose Your Operating Area</option>
-          <option className="dropdownStyle" value="North">North Operation Area</option>
-          <option className="dropdownStyle" value="Central">Central Operation Area</option>
-          <option className="dropdownStyle" value="South">South Operation Area</option>
-        </select>
+        <div className="selection">
+          <select className="dropdownStyle" ref="opsArea">
+            <option
+              className="dropdownStyle"
+              value=""
+              disable="true"
+              selected
+              hidden
+            >
+              Choose Your Operating Area
+            </option>
+            <option className="dropdownStyle" value="North">
+              North Operation Area
+            </option>
+            <option className="dropdownStyle" value="Central">
+              Central Operation Area
+            </option>
+            <option className="dropdownStyle" value="South">
+              South Operation Area
+            </option>
+          </select>
+        </div>
+        <div className="selection">
+          <select className="dropdownStyle" ref="hangarPickup">
+            <option
+              className="dropdownStyle"
+              value=""
+              disable="true"
+              selected
+              hidden
+            >
+              Choose Your Pick Up Location
+            </option>
+            <option className="dropdownStyle" value="Heber">
+              Heber Hanger
+            </option>
+            <option className="dropdownStyle" value="NSL">
+              North Salt Lake Hanger
+            </option>
+            <option className="dropdownStyle" value="Other">
+              Other
+            </option>
+          </select>
+        </div>
+        <div className="selection">
+          <select className="dropdownStyle" ref="guest">
+            <option
+              className="dropdownStyle"
+              value=""
+              disable="true"
+              selected
+              hidden
+            >
+              Choose the amount of Guest
+            </option>
+            <option className="dropdownStyle" value="1">
+              1
+            </option>
+            <option className="dropdownStyle" value="2">
+              2
+            </option>
+            <option className="dropdownStyle" value="3">
+              3
+            </option>
+            <option className="dropdownStyle" value="4">
+              4
+            </option>
+            <option className="dropdownStyle" value="5">
+              5
+            </option>
+            <option className="dropdownStyle" value="6">
+              6
+            </option>
+            <option className="dropdownStyle" value="7">
+              7
+            </option>
+            <option className="dropdownStyle" value="8">
+              8
+            </option>
+          </select>
+        </div>
+        <div className="selection">
+          <select className="dropdownStyle" ref="pickupTime">
+            <option disable="true" selected hidden>
+              AM or PM
+            </option>
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
+          </select>
+        </div>
+        <Calendar
+          style={style}
+          width="402px"
+          onDayClick={(e, day) => this.onDayClick(e, day)}
+        />
+        <div>
+          <div>
+            <button onClick={this.newRes}>Lets Fly!</button>
+          </div>
+          <Reservation />
+        </div>
       </div>
-      <div className="selection">
-        <select className="dropdownStyle"ref="hangarPickup">
-          <option className="dropdownStyle" value=""disable="true" selected hidden>Choose Your Pick Up Location</option>
-          <option className="dropdownStyle" value="Heber">Heber Hanger</option>
-          <option className="dropdownStyle" value="NSL">North Salt Lake Hanger</option>
-          <option className="dropdownStyle" value="Other">Other</option>
-        </select>
-      </div>
-      <div className="selection">
-        <select className="dropdownStyle"ref="guest">
-          <option  className="dropdownStyle" value=""disable="true" selected hidden>Choose the amount of Guest</option>
-          <option  className="dropdownStyle" value="1">1</option>
-          <option  className="dropdownStyle" value="2">2</option>
-          <option  className="dropdownStyle" value="3">3</option>
-          <option  className="dropdownStyle" value="4">4</option>
-          <option  className="dropdownStyle" value="5">5</option>
-          <option  className="dropdownStyle" value="6">6</option>
-          <option  className="dropdownStyle" value="7">7</option>
-          <option  className="dropdownStyle" value="8">8</option>
-        </select>
-      </div>
-      <div className="selection">
-        <select className="dropdownStyle"ref="pickupTime">
-          <option disable="true" selected hidden>AM or PM</option>
-          <option value="AM">AM</option>
-          <option value="PM">PM</option>
-        </select>
-      </div>
-      <Calendar style={style}
-                width="402px"
-                onChange={this.onChange}
-                getDate={this.getDate}
-                onClickYear={this.onClickYear}
-              />
-      <div>
-        <div><button onClick={this.newRes} >Lets Fly!</button></div>
-          <Reservation/>
-      </div>
-      </div>
-    )
+    );
   }
 }
 export default ResPage;
